d94a08257811450ba86f950dfc1ba6f342f3249d
Saqib-606 <saqi10606@gmail.com>
2026-04-30 19:57:11 +0500

    Initial Project Setup

---
+++ android/app/src/main/kotlin/com/example/nayapay_app_clone/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.nayapay_app_clone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+// NayaPay App Clone
+import 'package:flutter/material.dart'; 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp (
+      debugShowCheckedModeBanner: false,
+      home: const HomeScreen(), 
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen ({super.key});
+  @override
+  State <HomeScreen> createState () => _HomeScreenState ();
+}
+
+class _HomeScreenState extends State <HomeScreen> {
+  @override
+  Widget build (BuildContext context) {
+    return Scaffold (
+      body: Center (
+        child: Text ("Getting Started With NayaPay App Clone"),
+      )
+    );
+  }
+}
